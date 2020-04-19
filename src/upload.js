955461cb801d676c10586c8182481f2ea6db3684
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-19 19:56:50 +0800

    Add usage message of the upload program

--- src/upload.js
+++ src/upload.js
@@ -3,9 +3,26 @@ const handleURL = require('./handleURL');
 const handleFile = require('./handleFile');
 const uploadImageToGithubRepo = require('./uploadImageToGithubRepo');
 
+const usage = `
+usage: upload [parameters]... 
+
+available parameters:
+    --url       web image url
+    --file      local file path
+
+eg.
+    upload --url https://host.com/static/images/image.jpg
+    upload --file ~/path-to-your-image/images/image.jpg
+`
+
 const upload = async () => {
   try {
     const { file, url } = minimist(process.argv);
+    if (!file && !url) {
+      console.log(usage);
+      console.error('You need to provide at least one between FILE and URL.')
+      return;
+    }
     await handleURL(url);
     await handleFile(file);
     await uploadImageToGithubRepo();
