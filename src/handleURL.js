8b63ab218cdcf019b66caabdc208165275d00785
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-19 23:05:38 +0800

    WIP

--- src/handleURL.js
+++ src/handleURL.js
@@ -1,7 +1,19 @@
-const handleURL = url => {
+const downloader = require('image-downloader')
+const path = require('path');
+
+const handleURL = async url => {
   if (!url) {
     return;
   }
+  const options = {
+    url,
+    dest: path.resolve(__dirname, '../images'),
+  }
+  return downloader.image(options)
+    .then(({ filename, image }) => {
+      console.log('The image is saved to', filename);
+    })
+    .catch(console.error)
 };
 
 module.exports = handleURL;
