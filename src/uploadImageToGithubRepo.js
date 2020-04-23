6f83d17ed44b95c79070b9abafc3c4ba36e4b446
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-23 23:27:05 +0800

    Add basic auto steps

--- src/uploadImageToGithubRepo.js
+++ src/uploadImageToGithubRepo.js
@@ -2,7 +2,15 @@ const sh = require('./utils/sh');
 
 const uploadImageToGithubRepo = () => {
   return sh('git add --all;git commit -m "Add basic auto steps"').then(output => {
-    console.log(output);
+    if (output && output.stdout) {
+      console.log(output.stdout);
+    }
+    if (output && output.stderr) {
+      console.error(output.stderr);
+    }
+    if (!output.stderr && !output.stdout) {
+      console.log(output);
+    }
   });
 };
 
