3c07ef6b81be59f6f3a722034996e4f241df2e9a
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-24 20:34:16 +0800

    Print the uploaded file url

--- src/handleURL.js
+++ src/handleURL.js
@@ -7,16 +7,21 @@ const handleURL = async url => {
   if (!url) {
     return;
   }
-  const dir = path.resolve(__dirname, `../images/v2/${new Date().getTime()}`)
+  const timestamp = new Date().getTime();
+  const imageUUID = uuid();
+  const imageFormat = extractFormat(url);
+  const dir = path.resolve(__dirname, `../images/v2/${timestamp}`);
+  const name = `${imageUUID}.${imageFormat}`;
   await sh(`mkdir -p ${dir}`);
   const options = {
     url,
-    dest: path.resolve(__dirname, `${dir}/${uuid()}.${extractFormat(url)}`),
+    dest: path.resolve(__dirname, `${dir}/${name}`),
     extractFilename: false,
   }
   return downloader.image(options)
     .then(({ filename, image }) => {
       console.log('The image is saved to', filename);
+      console.log('The image is at ', `https://raw.githubusercontent.com/emagrorrim/static-images/master/images/v2/${timestamp}/${name}`);
     })
 };
 
