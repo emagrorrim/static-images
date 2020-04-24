4fd3a0427fea7db3bfb66e29124920a92ca535b4
Xin Guo <emagrorrimxguo@gmail.com>
2020-04-24 20:42:48 +0800

    Add image

--- src/uploadImageToGithubRepo.js
+++ src/uploadImageToGithubRepo.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const sh = require('./utils/sh');
 
+const command = `
+  git add --all; \
+  git commit -m "Add image"; \
+  git push origin $(sh ${path.resolve(__dirname, './scripts/git_current_branch.sh')});
+`
+
 const uploadImageToGithubRepo = () => {
-  return sh('git add --all;git commit -m "Add image";git push origin $(sh ./scripts/git_current_branch)').then(output => {
+  return sh(command).then(output => {
     if (output && output.stdout) {
       console.log(output.stdout);
     }
